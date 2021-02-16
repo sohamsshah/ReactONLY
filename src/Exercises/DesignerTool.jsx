c9d4833518e8553a1366f8748b37880c8d74ad27
Soham Shah <47717492+sohamsshah@users.noreply.github.com>
2021-02-16 11:42:02 +0530

    complete DesignerTool

--- src/Exercises/DesignerTool.jsx
+++ src/Exercises/DesignerTool.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useState } from "react";
+import "../styles.css";
+
+const fontDictionary = {
+  "Bree Serif": "Bree Serif, serif",
+  Roboto: "Roboto, sans-serif",
+  Poppins: "Poppins, sans-serif",
+  Anton: "Anton, sans-serif"
+};
 
 export default function DesignerTool() {
   const [size, setSize] = useState(10);
+  const [font, setFont] = useState("Bree Serif");
 
   function alterSize(size) {
     document.querySelector(".text").style.fontSize = `${size}px`;
   }
+
   function incrementSize() {
-    console.log(size);
     if (size >= 10 && size < 100) {
       setSize(size + 2);
       alterSize(size);
@@ -19,19 +28,33 @@ export default function DesignerTool() {
       setSize(size - 2);
       alterSize(size);
     }
+  }
 
-    console.log(size);
+  function alterFont() {
+    setFont(document.getElementById("fonts").value);
   }
 
   return (
     <div className="App">
       <button onClick={incrementSize} className="increment">
         +
-      </button>
-      <h1 className="text"> Soham</h1>
+      </button>{" "}
       <button onClick={decrementSize} className="decrement">
-        -
-      </button>
+        -{console.log(size)}
+      </button>{" "}
+      <select onChange={alterFont} name="fonts" id="fonts">
+        <option value="Bree Serif">Bree Serif</option>
+        <option value="Roboto">Roboto</option>
+        <option value="Anton">Anton</option>
+        <option value="Poppins">Poppins</option>
+      </select>
+      <h1
+        style={{ fontSize: `${size}px`, fontFamily: fontDictionary[font] }}
+        className="text"
+      >
+        {" "}
+        Soham
+      </h1>
     </div>
   );
 }
