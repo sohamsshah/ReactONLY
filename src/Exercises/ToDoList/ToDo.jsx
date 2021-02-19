7eb99242bfa1ee3edbb4e0aed7738d5cb88592ac
Soham Shah <47717492+sohamsshah@users.noreply.github.com>
2021-02-19 17:45:51 +0530

    complete ToDo functionality

--- src/Exercises/ToDoList/ToDo.jsx
+++ src/Exercises/ToDoList/ToDo.jsx
@@ -2,30 +2,49 @@ import React from "react";
 import { useState } from "react";
 
 export default function ToDo() {
-  const [items, setItems] = useState(["Item1", "Item2", "Item3"]);
-  function updateToDo(item, op) {
-    if (op === "-") {
-      items.splice(item, 1);
-      setItems([...items]);
-    } else if (op === "+") {
-      items.append();
+  const [items, setItems] = useState({
+    Item1: false,
+    Item2: false,
+    Item3: false
+  });
+  function updateToDo(op, item) {
+    if (op === "+") {
+      let newItem = document.querySelector(".input").value;
+      setItems({ ...items, [newItem]: false });
+    } else if (op === "-") {
+      delete items[item];
+      setItems({ ...items });
     }
+  }
+
+  function updateText(item) {
+    setItems({ ...items, [item]: !items[item] });
   }
   return (
     <div className="App">
       <h1> To Do List</h1>
-      <div>
+      <div stye={{ padding: "2rem 40%" }}>
         <div>
-          <input type="text" />
-          <button onClick={() => updateToDo(items.length - 1, "-")}>Add</button>
+          <input className="input" type="text" />
+          <button onClick={() => updateToDo("+")}>Add</button>
         </div>
         <div>
-          <ul>
-            {items.map((item, ind) => (
+          <ul style={{ listStyleType: "none" }}>
+            {Object.keys(items).map((item) => (
               <li>
                 <div>
-                  <input type="checkbox" />
-                  {item} <button onClick={() => updateToDo(ind, "-")}>X</button>
+                  <input type="checkbox" onChange={() => updateText(item)} />
+                  {items[item] ? (
+                    <span
+                      className="text"
+                      style={{ textDecoration: "line-through" }}
+                    >
+                      {item}
+                    </span>
+                  ) : (
+                    <span className="text">{item}</span>
+                  )}
+                  <button onClick={() => updateToDo("-", item)}>X</button>
                 </div>
               </li>
             ))}
